441057ee2aab9e52174a49200cac4b47f6d3f709
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-03-21 01:25:51 +0530

    Updated frontend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import { ethers } from 'ethers';
 import { Link } from 'react-router-dom'
 
+import './styles.css'
+
 const Navbar = ({ web3Handler, account }) => {
     if (account) {
         return (
             <div className="Navbar">
-                <h2 className='Navbar-logo'>Rabbit$</h2>
+                <p className='Navbar-logo'>Rabbit$</p>
                 <Link className='Navbar-signedin' onClick={web3Handler}>{account.slice(0, 5)+'...'}</Link>
             </div>
         )
@@ -14,7 +16,7 @@ const Navbar = ({ web3Handler, account }) => {
 
     return (
         <div className="Navbar">
-            <h2 className='Navbar-logo'>Rabbit$</h2>
+            <p className='Navbar-logo'>Rabbit$</p>
             <Link className='Navbar-signin' onClick={web3Handler}>Connect wallet</Link>
         </div>
     )
