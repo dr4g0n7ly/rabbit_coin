79734b959bf483b7019457d47dccad8cab462b94
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-03-24 02:19:16 +0530

    IPFS trials

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import { ethers } from 'ethers'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AccountProvider } from './AccountContext'
 
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Upload from './components/Upload'
 
 import './App.css';
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [tokenContract, setTokenContract] = useState({})
 
   return (
     <AccountProvider>
@@ -21,7 +18,7 @@ function App() {
           <br/>
           <Navbar/>
           <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/home" element={<Home/>} />
           </Routes>
         </div>
       </BrowserRouter>
