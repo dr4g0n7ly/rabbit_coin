3111f29e08914a117162587b69aef556fc7d1f8d
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-03-24 18:31:30 +0530

    Redeploy smart contract

--- client/src/components/Transactions.js
+++ client/src/components/Transactions.js
@@ -1,62 +1,228 @@
-const Transactions = () => {
-    return (
-        <div className="Transactions">
-            <div className="Main">
-                <p className="main-h1">Transactions</p>
-                <div className="main-stats">
-                    <div className="main-stat-child">
-                        <p className="stat-child-0">Last</p>
-                        <p className="stat-child-0">30 days</p>
-                    </div>
-                    <div className="main-stat-child">
-                        <p className="stat-child-1">TRANSACTIONS</p>
-                        <p className="stat-child-2">30 days</p>
-                    </div>
-                    <div className="main-stat-child">
-                        <p className="stat-child-1">TOTAL EXPENSES</p>
-                        <p className="stat-child-2">6843909 RBT</p>
-                    </div>
-                    <div className="main-stat-child">
-                        <p className="stat-child-1">TOTAL INCOME</p>
-                        <p className="stat-child-2">8939390 RBT</p>
-                    </div>
-                </div>
-                <div className="main-trs-heads">
-                    <div className="main-trs-head block-col">
-                        <p className="trs-head">Block</p>
-                    </div>
-                    <div className="main-trs-head">
-                        <p className="trs-head">Address</p>
-                    </div>
-                    <div className="main-trs-head">
-                        <p className="trs-head">Type</p>
-                    </div>
-                    <div className="main-trs-head">
-                        <p className="trs-head">Amount</p>
-                    </div>
-                </div>
+import { useState, useEffect, useContext } from 'react'
+import { AccountContext } from '../AccountContext'
 
-                <div className="transaction-list">
-                    <div className="trs-row">
-                        <div className="trs-el block-col">
-                            <p className="">Block</p>
+import RabbitCoinJSON from '../RabbitCoin.json'
+
+const Transactions = () => {
+
+    const [balance, setBalance] = useState(0)
+    const {account} = useContext(AccountContext)
+
+    const [depositAmount, setDepositAmount] = useState(0)
+
+    const [withdrawAmount, setWithdrawAmount] = useState(0)
+
+    const [transferAmount, setTransferAmount] = useState(0)
+    const [transferReciever, setTransferReciever] = useState('')
+
+    const getBalance = async () => {
+        const ethers = require("ethers");
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        let contract = new ethers.Contract(RabbitCoinJSON.address, RabbitCoinJSON.abi, signer)
+ 
+        let bal = await contract.balanceOf(account)
+        console.log("balance: "+ bal)
+        setBalance(parseInt(bal._hex))
+    }
+
+    useEffect(() => {
+        getBalance()
+    })
+
+    const handleDepositSubmit = async (e) => {
+        e.preventDefault()
+        console.log('handledepositsubmit')
+        const ethers = require("ethers")
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        let contract = new ethers.Contract(RabbitCoinJSON.address, RabbitCoinJSON.abi, signer)
+
+        const depositPriceString = depositAmount.toString() + "000000000000"
+
+        let depositComplete = await contract.deposit(depositAmount.toString(), {value: depositPriceString})
+        console.log("depositComplete: ", depositComplete)
+    }
+
+    const handleWithdrawSubmit = async (e) => {
+        e.preventDefault()
+        const ethers = require("ethers")
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        let contract = new ethers.Contract(RabbitCoinJSON.address, RabbitCoinJSON.abi, signer)
+
+        const withdrawAmountString = ethers.utils.parseUnits(withdrawAmount.toString(),18)
+
+        let withdrawComplete = await contract.withdraw(withdrawAmountString, {gasLimit: 3e7})
+        console.log("withdrawComplete: ", withdrawComplete)
+    }
+
+    const handleTransferSubmit = async (e) => {
+        e.preventDefault()
+        const ethers = require("ethers");
+        
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        let contract = new ethers.Contract(RabbitCoinJSON.address, RabbitCoinJSON.abi, signer)
+
+        const transferAmountString = ethers.utils.parseUnits(transferAmount.toString(),18)
+
+        let transferComplete = await contract.transfer(transferReciever, transferAmountString)
+        console.log("transferComplete: ", transferComplete)
+    }
+
+    if (account) {
+        return (
+            <div className="Transactions">
+                <div className="Main">
+                    <p className="main-h1">Transactions</p>
+                    <div className="main-stats">
+                        <div className="main-stat-child">
+                            <p className="stat-child-0">Last</p>
+                            <p className="stat-child-0">30 days</p>
                         </div>
-                        <div className="trs-el">
-                            <p className="">Address</p>
+                        <div className="main-stat-child">
+                            <p className="stat-child-1">TRANSACTIONS</p>
+                            <p className="stat-child-2">30 days</p>
                         </div>
-                        <div className="trs-el">
-                            <p className="">Type</p>
+                        <div className="main-stat-child">
+                            <p className="stat-child-1">TOTAL EXPENSES</p>
+                            <p className="stat-child-2">6843909 RBT</p>
                         </div>
-                        <div className="trs-el">
-                            <p className="">Amount</p>
+                        <div className="main-stat-child">
+                            <p className="stat-child-1">TOTAL INCOME</p>
+                            <p className="stat-child-2">8939390 RBT</p>
                         </div>
                     </div>
+                    <div className="main-trs-heads">
+                        <div className="main-trs-head block-col">
+                            <p className="trs-head">Block</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head">Address</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head">Type</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head">Amount</p>
+                        </div>
+                    </div>
+
+                    <div className="transaction-list">
+                        <div className="trs-row">
+                            <div className="trs-el block-col">
+                                <p className="">Block</p>
+                            </div>
+                            <div className="trs-el">
+                                <p className="">Address</p>
+                            </div>
+                            <div className="trs-el">
+                                <p className="">Type</p>
+                            </div>
+                            <div className="trs-el">
+                                <p className="">Amount</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                
+                <div className="Funds">
+                    <p>Balance</p>
+                    <p>{balance}</p>
+
+                    <br/>
+                    <br/>
+
+                    <section>
+
+                        <p className="home-transaction-h1">Deposit</p>
+                        <br/>
+                        <form onSubmit={handleDepositSubmit}>
+
+                            <label htmlFor='amount' className='label'>Enter amount:</label>
+                            <input 
+                                id="deposit-amount" 
+                                type="number"
+                                onChange={(e) => setDepositAmount(e.target.value)}
+                                value = { depositAmount }
+                                required
+                                autoFocus
+                            />
+
+                            <button>Deposit ETH</button>
+
+                        </form>
+
+                        <br/>
+                        <br/>
+
+                        <p className="home-transaction-h1">Withdraw</p>
+                        <br/>
+                        <form onSubmit={handleWithdrawSubmit}>
+
+                            <label htmlFor='amount' className='label'>Enter amount:</label>
+                            <input 
+                                id="withdraw-amount" 
+                                type="number"
+                                onChange={(e) => setWithdrawAmount(e.target.value)}
+                                value = { withdrawAmount }
+                                required
+                                autoFocus
+                            />
+
+                            <button>Withdraw ETH</button>
+
+                        </form>
+
+                        <br/>
+                        <br/>
+
+                        <p className="home-transaction-h1">Transfer</p>
+                        <br/>
+                        <form onSubmit={handleTransferSubmit}>
+
+                            <label htmlFor='amount' className='label'>Enter amount:</label>
+                            <input 
+                                id="transfer-amount" 
+                                type="number"
+                                onChange={(e) => setTransferAmount(e.target.value)}
+                                value = { transferAmount }
+                                required
+                                autoFocus
+                            />
+
+                            <label htmlFor='reciever' className='label'>Enter reciever address:</label>
+                            <input
+                                id="transfer-reciever"
+                                type="text"
+                                onChange={(e) => setTransferReciever(e.target.value)}
+                                value = { transferReciever }
+                                required
+                                autoFocus
+                            />  
+
+                            <button>Transfer RBT</button>
+
+                        </form>
+
+                    </section>
+                </div>
             </div>
-            <div className="Funds">Deposit</div>
-        </div>
-    )
+        )
+    }
+    else {
+        return (
+            <div>
+                <h1>PLEASE CONNECT ACCOUNT</h1>
+            </div>
+        )
+    }
 }
 
 export default Transactions
--- contracts/RabbitCoin.sol
+++ contracts/RabbitCoin.sol
@@ -7,13 +7,9 @@ contract RabbitCoin {
     string private constant _name = "RabbitCoin";
     string private constant _symbol = "RBT";
 
-    uint256 private constant _totalSupply = 10000000000 * 10 ** 18;
-    uint8 private constant _decimals = 18;    uint256 private constant _price = 100000000000000 wei;
+    uint256 private constant _totalSupply = 1000 * 10 ** 18;
 
-    uint256 public _totalDepositAmount;
-    uint256 public _totalLoans;
-    uint256 public _totalLoanAmount;
-    uint256 public _totalProfit;
+    uint256 private constant _price = 1000000000000 wei;
 
     mapping(address => uint256) public balances;
 
@@ -28,11 +24,6 @@ contract RabbitCoin {
     constructor() {
         minter = msg.sender;
         mint(_totalSupply);
-
-        _totalDepositAmount = 0;
-        _totalLoans = 0;
-        _totalLoanAmount = 0;
-        _totalProfit = 0;
     }
 
 
@@ -50,10 +41,6 @@ contract RabbitCoin {
        return _totalSupply;
     }
 
-    function decimals() public pure returns (uint8) {
-        return _decimals;
-    }
-
 
     // READ ---------
 
@@ -61,17 +48,13 @@ contract RabbitCoin {
         return balances[tokenOwner_];
     }
 
-    function getDepositAmount() public view returns (uint256) {
-        return _totalDepositAmount;
-    }
-
 
     // WRITE --------
 
     error InsufficientBalance(uint256 requested, uint256 available);
 
-    event Transfered(address from, address to, uint256 amount, uint256 price_Eth, bytes32 transaction_type);
 
+    event Transfered(address from, address to, uint256 amount);
 
     function transfer(address receiver_, uint256 amount_) public returns (bool) {
         if(amount_ > balances[msg.sender]) {
@@ -84,11 +67,13 @@ contract RabbitCoin {
         balances[msg.sender] -= amount_;
         balances[receiver_] += amount_;
 
-        emit Transfered(msg.sender, receiver_, amount_, 0, "transfer");
+        emit Transfered(msg.sender, receiver_, amount_);
 
         return true;
     }
 
+
+    event Deposited(address from, uint256 amount, uint256 price);
 
     function deposit(uint256 amount_) public payable returns (bool) {
         require(msg.value == _price * amount_, "Insufficient Ethereum");
@@ -96,13 +81,13 @@ contract RabbitCoin {
         balances[msg.sender] += amount_;
         balances[minter] -= amount_;
 
-        _totalDepositAmount += msg.value;
-
-        emit Transfered(minter, msg.sender, amount_, msg.value, "deposit");
+        emit Deposited(msg.sender, amount_, msg.value);
 
         return true;
     }
 
+
+    event Withdrew(address from, uint256 amount, uint256 price);
 
     function withdraw(uint amount_) public returns (bool) {
 
@@ -117,11 +102,8 @@ contract RabbitCoin {
         balances[minter] += amount_;
 
         payable(msg.sender).transfer(amount_ * _price);
-
-        _totalDepositAmount -= amount_ * _price;
-
-        emit Transfered(msg.sender, minter, amount_, amount_ * _price, "withdraw");
         
         return true;
     }
+
 }
