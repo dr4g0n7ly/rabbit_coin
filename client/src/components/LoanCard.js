2325b0e685b75d342fa1acd1c705227554863d4c
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-03-26 02:42:21 +0530

    Loan Cards completed

--- client/src/components/LoanCard.js
+++ client/src/components/LoanCard.js
@@ -1,11 +1,105 @@
 const LoanCard = (props) => {
-    return (
-        <div className="account-card">
-            <p className="acc-num">Status: {props.digits}</p>
-            <p className="acc-num">Loan amount: {props.digits}</p>
-            <p className="acc-num">Total pay-off amount: {props.digits}</p>
-            <p className="acc-num">Due block: {props.digits}</p>
-        </div>
-    )
+
+    var status = null;
+    var button = null;
+    var buttonFunction = null;
+
+    if (props.status == 1) {
+        return (
+            <div className="loan-row">
+                <div className="loan-el block-col">
+                    <p className="">requested</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.loanAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.payoffAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.dueBlock}</p>
+                </div>
+                <button className="loan-el-button">Cancel Loan</button>
+            </div>
+        )
+    } else if (props.status == 2) {
+        return (
+            <div className="loan-row">
+                <div className="loan-el block-col">
+                    <p className="">accepted</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.loanAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.payoffAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.dueBlock}</p>
+                </div>
+                <button className="loan-el-button">Pay Loan</button>
+            </div>
+        )
+    } else if (props.status == 3) {
+        return (
+            <div className="loan-row">
+                <div className="loan-el block-col">
+                    <p className="">paid</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.loanAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.payoffAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.dueBlock}</p>
+                </div>
+                <div className="loan-el" style={{textAlign:'center'}}>
+                    <p className="">-</p>
+                </div>
+            </div>
+        )
+    } else if (props.status == 4) {
+        return (
+            <div className="loan-row">
+                <div className="loan-el block-col">
+                    <p className="">Defaulted</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.loanAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.payoffAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.dueBlock}</p>
+                </div>
+                <div className="loan-el" style={{textAlign:'center'}}>
+                    <p className="">-</p>
+                </div>
+            </div>
+        )
+    } else if (props.status == 5) {
+        return (
+            <div className="loan-row">
+                <div className="loan-el block-col">
+                    <p className="">Cancelled</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.loanAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.payoffAmount} Eth</p>
+                </div>
+                <div className="loan-el">
+                    <p className="">{props.dueBlock}</p>
+                </div>
+                <div className="loan-el" style={{textAlign:'center'}}>
+                    <p className="">-</p>
+                </div>
+            </div>
+        )
+    }
 }
 export default LoanCard
--- client/src/components/Loans.js
+++ client/src/components/Loans.js
@@ -1,18 +1,20 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+import { AccountContext } from '../AccountContext'
 import LoanForm from "./LoanForm"
 import LoanCard from "./LoanCard"
 
 import RabbitCoinJSON from '../RabbitCoin.json'
 import refresh from './public/icons/refresh.png'
+import { json } from "react-router-dom"
 
 const Loans = () => {
 
     const [currentBlock, setCurrentBlock] = useState()
-    const [loans, setLoans] = useState([])
+    const [loanData, setLoanData] = useState([])
+    const {account} = useContext(AccountContext)
 
     const getCurrentBlock = async () => {
         const ethers = require("ethers");
-
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
 
@@ -21,9 +23,51 @@ const Loans = () => {
         let block = await contract.getCurrentBlock()
         setCurrentBlock(parseInt(block._hex))
     }
+  
+    const getIPFSGatewayURL = (ipfsURL)=>{
+      let urlArray = ipfsURL.split("/");
+      let ipfsGateWayURL = `https://${urlArray[2]}.ipfs.dweb.link/${urlArray[3]}`;
+      return ipfsGateWayURL;
+    }
+  
+    // const previewNFT = ({data}) =>{
+    //   console.log(data.data.image);
+    //   let imgViewString = getIPFSGatewayURL(data.data.image.pathname);
+    //   setImageView(imgViewString);
+    // }
+  
+    async function getAllLoans() {
+        const ethers = require("ethers");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const connectedContract = new ethers.Contract(
+            RabbitCoinJSON.address,
+            RabbitCoinJSON.abi,
+            provider.getSigner()
+        );
+        let allLoans = await connectedContract.getAllLoans()
+        const items = await Promise.all(allLoans.map(async i => {
+            const tokenURI = await connectedContract.getTokenURI(i.tokenId);
+            const metaDataURL = getIPFSGatewayURL(tokenURI)
+            console.log("tokenURL", metaDataURL)
+            console.log("i: ", i)
 
+            let item = {
+                tokenId: i.tokenId.toString(),
+                borrower: i.borrower,
+                status: i.status,
+                loanAmount: ethers.utils.formatUnits(i.principle.toString(), 'ether'),
+                payoffAmount: ethers.utils.formatUnits(i.totalPayback.toString(), 'ether'),
+                dueBlock: i.dueDate.toString()
+            }
+            console.log(item)
+            return item
+        }))
+        setLoanData(items);
+    }
+  
     useEffect(() => {
         getCurrentBlock()
+        getAllLoans()
     }, [])
 
     return (
@@ -39,14 +83,34 @@ const Loans = () => {
                         <p className="block-NUM">{currentBlock}</p>
                     </div>
                 </div>
-                <div className="account-cards">
-                    {loans.map((acc)=>{
-                        return (
-                        <LoanCard acc={acc} name={acc.name} digits={acc.digits} balance={acc.balance} id={acc.id}/>
-                    );})}
-                    <LoanCard digits={"3400"}/>
-                    <LoanCard digits={"3400"}/>
-                </div>
+                <div className="loan-trs-heads">
+                        <div className="main-trs-head block-col">
+                            <p className="trs-head">Status</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head">Loan Amount</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head">Payable Amount</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head">Due block</p>
+                        </div>
+                        <div className="main-trs-head">
+                            <p className="trs-head" style={{textAlign:'center'}}>Actions</p>
+                        </div>
+                    </div>
+                    <div className='trs-head-line'></div>
+
+                    <div className="transaction-list">
+                        {loanData.map((loan) => {
+                            if (JSON.stringify(account).toLowerCase() === JSON.stringify(loan.borrower).toLowerCase()) {
+                                return (
+                                    <LoanCard key={loan.tokenId} status={loan.status} loanAmount={loan.loanAmount} payoffAmount={loan.payoffAmount} dueBlock={loan.dueBlock}/>
+                                )
+                            }
+                        })}
+                    </div>
             </div>
             <LoanForm/>
         </div>
